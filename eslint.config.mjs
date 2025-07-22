fb73435103d3b2a48e245a7e4ab7d55ffeb02fc6
komiljohn <ykomiljohn@gmail.com>
2025-07-22 07:12:52 +0500

    setup eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import prettierConfig from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +14,20 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  prettierConfig,
+  {
+    files: ["**/*.{ts,tsx,js}"],
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "prettier/prettier": "error",
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "@typescript-eslint/no-unused-vars": "error",
+    },
+  },
 ];
 
 export default eslintConfig;
